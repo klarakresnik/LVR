a4785194b6014854e2c977ebe389356d287ba6fc
klarakresnik <64692261+klarakresnik@users.noreply.github.com>
2024-06-13 20:50:24 +0200

    končna verzija

--- project/Project.lean
+++ project/Project.lean
@@ -1,106 +1,101 @@
--- This module serves as the root of the `Project` library.
--- Import modules here that should be built as part of the library.
-import «Project».Basic
 import Mathlib
+import Mathlib.Data.Nat.Choose.Basic
+import Mathlib.Tactic
 
-/- The type of binary trees -/
+open BigOperators
 
-inductive binary_tree : Type
-| leaf : binary_tree
-| node₁ : binary_tree → binary_tree
-| node₂ : binary_tree → binary_tree → binary_tree
+-- 1. SMALL TASK
+-- Formalize the recursive definition
+-- C_0 = 0 and C_{n+1} = ∑_i=0^n C_i C_{n-i}
+-- of the catalan numbers.
+def catalan_number: ℕ → ℕ
+| 0 => 1
+| n+1 => ∑ i : Fin (n+1), (catalan_number i) * (catalan_number (n-i))
 
-
-/- The height of a binary tree -/
-
-def binary_tree.height : binary_tree → ℕ
-| .leaf => 1
-| .node₁ T => T.height + 1
-| .node₂ T1 T2 => max T1.height T2.height + 1
-
-
-/- The number of nodes (including leaves) of a binary tree -/
-
-def binary_tree.number_of_nodes : binary_tree → ℕ
-| .leaf => 1
-| .node₁ T => T.number_of_nodes + 1
-| .node₂ T1 T2 => T1.number_of_nodes + T2.number_of_nodes + 1
+-- 2. SMALL TASK
+-- Formalize the concept of plane trees.
+inductive plane_tree : Type
+| node : List plane_tree → plane_tree
 
 
-/- The type of full binary tree -/
-
+-- 3. SMALL TASK
+-- Formalize the concept of full binary trees.
 inductive full_binary_tree: Type
 | leaf : full_binary_tree
 | node₂ : (T1 T2 : full_binary_tree) → full_binary_tree
 
-
-/- The height of a full binary tree -/
-
-def full_binary_tree.height : full_binary_tree → ℕ
-| .leaf => 1
-| .node₂ T1 T2 => max T1.height T2.height + 1
-
-
-/- The number of nodes including leaves of a full binary tree -/
-
-def full_binary_tree.number_of_nodes : full_binary_tree → ℕ
-| .leaf => 1
-| .node₂ T1 T2 => T1.number_of_nodes + T2.number_of_nodes + 1
-
-
-/- Function that converts full binary tree to binary tree -/
-
-def binary_tree_of_full_binary_tree : full_binary_tree → binary_tree
-| .leaf => .leaf -- pika se navezuje na prvi argument (oba imata konstruktor leaf)
-| .node₂ T1 T2 => .node₂ (binary_tree_of_full_binary_tree T1) (binary_tree_of_full_binary_tree T2)
-
-theorem eq_height_binary_tree_of_full_binary_tree :
-(T : full_binary_tree) → T.height = (binary_tree_of_full_binary_tree T).height := by -- by pomeni da začnemo dokaz
-intro T
-induction T with
-| leaf => rfl
-| node₂ T1 T2 HT1 HT2 =>
-  simp [full_binary_tree.height, binary_tree.height] -- simp je unfoldu definition height
-  rw[HT1, HT2]
-
-theorem eq_number_of_nodes_binary_tree_of_full_binary_tree :
-(T : full_binary_tree) → T.number_of_nodes = (binary_tree_of_full_binary_tree T).number_of_nodes := by
-intro T
-induction T with
-| leaf => rfl
-| node₂ T1 T2 HT1 HT2 =>
-  simp[full_binary_tree.number_of_nodes, binary_tree.number_of_nodes]
-  rw[HT1, HT2]
-
+-- 4. SMALL TASK
+-- Construct the type of full binary trees with n nodes, not counting the leaves.
 inductive binary_tree_with_nodes : (n : ℕ) → Type
-| leaf : binary_tree_with_nodes 1
+| leaf : binary_tree_with_nodes 0
 | node₁ : {n : ℕ} → binary_tree_with_nodes n → binary_tree_with_nodes (n + 1)
-| node₂ : {m n : ℕ} → binary_tree_with_nodes m → binary_tree_with_nodes n → binary_tree_with_nodes (m + n)
+| node₂ : {m n : ℕ} → binary_tree_with_nodes m → binary_tree_with_nodes n → binary_tree_with_nodes (m + n + 1)
 
+-- 5. SMALL TASK
+-- Define the type of ballot sequences of length n.
+inductive ballot_sequence : ℕ → ℕ → Type
+| empty : ballot_sequence 0 0
+| add_zero : {zeros ones : ℕ} → ballot_sequence zeros ones → ballot_sequence (zeros + 1) ones
+| add_one : {zeros ones : ℕ} → ballot_sequence zeros ones → zeros > ones → ballot_sequence zeros (ones + 1)
 
-/- The type of vectors of elements in a type A (aka labeled unary trees) -/
+-- 4. LARGE TASK
+-- Construct a bijection
+-- list PlaneTree ∼= PlaneTree
+def list_plane_tree_of_plane_tree : plane_tree → List plane_tree
+| .node l => l
 
-inductive vector (A : Type) : (n : ℕ) → Type -- A parameter (se ne spreminja), n argument (se spreminja)
-| nil : vector A 0
-| cons : (n : ℕ) → A → vector A n → vector A (n + 1)
+def plane_tree_of_list_plane_tree : List plane_tree → plane_tree
+| l => .node l
 
+theorem plane_tree_isomorphic_to_list_plane_tree :
+∀ (T : plane_tree),
+T = plane_tree_of_list_plane_tree (list_plane_tree_of_plane_tree T) := by
+intro T
+cases T
+simp[list_plane_tree_of_plane_tree]
+simp[plane_tree_of_list_plane_tree]
 
-/- A function converting binary trees with n nodes to ordinary binary trees -/
+theorem list_plane_tree_isomorphic_to_plane_tree :
+∀ (T : List plane_tree),
+T = list_plane_tree_of_plane_tree (plane_tree_of_list_plane_tree T) := by
+intro T
+simp[plane_tree_of_list_plane_tree, list_plane_tree_of_plane_tree]
 
-def binary_tree_of_binary_tree_with_nodes {n : ℕ} : binary_tree_with_nodes n → binary_tree
-| .leaf => .leaf
-| .node₁ T => .node₁ (binary_tree_of_binary_tree_with_nodes T)
-| .node₂ T1 T2 => .node₂ (binary_tree_of_binary_tree_with_nodes T1) (binary_tree_of_binary_tree_with_nodes T2)
+-- 5. LARGE TASK
+-- Construct the rotating isomorphism, which is the isomorphism between plane trees and full binary trees.
 
-theorem eq_number_of_nodes_binary_tree_of_full_binary_tree_with_nodes :
-∀ (n : ℕ)
- (T : binary_tree_with_nodes n), n = (binary_tree_of_binary_tree_with_nodes T).number_of_nodes := by
-  intros n T
-  induction T with
-  | leaf => rfl
-  | node_1 T HT1 =>
-    simp [binary_tree.number_of_nodes]
-    exact HT1
-  | node_2 T1 T2 HT1 HT2 =>
-    simp [binary_tree.number_of_nodes]
-    rw [<- HT1, <- HT2]
+def plane_tree_of_full_binary_tree : full_binary_tree → plane_tree
+| .leaf => .node []
+| .node₂ T1 T2 => .node (plane_tree_of_full_binary_tree T1 :: list_plane_tree_of_plane_tree (plane_tree_of_full_binary_tree T2))
+
+def full_binary_tree_of_plane_tree : plane_tree → full_binary_tree
+  | .node [] => .leaf
+  | .node (t1::ts) => .node₂ (full_binary_tree_of_plane_tree t1) (full_binary_tree_of_plane_tree (.node ts))
+
+theorem plane_tree_isomorphic_to_full_binary_tree :
+∀ (T : plane_tree), T = plane_tree_of_full_binary_tree (full_binary_tree_of_plane_tree T) := by
+rintro ⟨⟨⟩ | ⟨T , l⟩⟩
+· rfl
+· rw[full_binary_tree_of_plane_tree]
+  simp[plane_tree_of_full_binary_tree]
+  apply And.intro
+  rw[<-plane_tree_isomorphic_to_full_binary_tree]
+  rw[<-plane_tree_isomorphic_to_full_binary_tree]
+  rw[list_plane_tree_of_plane_tree]
+
+theorem full_binary_tree_isomorphic_to_plane_tree :
+∀ (T : full_binary_tree),
+T = full_binary_tree_of_plane_tree (plane_tree_of_full_binary_tree T) := by
+intro T
+induction T with
+| leaf =>
+  simp[plane_tree_of_full_binary_tree, full_binary_tree_of_plane_tree]
+| node₂ T1 T2 HT1 HT2 =>
+  simp[plane_tree_of_full_binary_tree, full_binary_tree_of_plane_tree]
+  apply And.intro
+  rw[<-HT1]
+  simp[list_plane_tree_of_plane_tree]
+  split
+  · case node₂ type h =>
+    rw[<- h]
+    rw[<- HT2]
